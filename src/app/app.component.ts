157e18948a718cbddafaf2460ae3452afddf59b0
Peter <peter.lakatos@outlook.de>
2023-05-29 18:56:58 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataTestService } from './data-test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private dataTestService: DataTestService){
+
+  }
   title = 'api-test';
+datas:any[] = [];
+  ngOnInit(): void {
+  let list = this.dataTestService.getData().subscribe(data => {
+this.datas = data;
+
+  });
+  
+  }
+
+
 }
